29511daeb47226447567d9dd78d47c5404d6a708
Ronald Arias <ariasron@hotmail.com>
2021-06-04 09:59:10 -0500

    Funcion saludar

--- ejemplo.js
+++ ejemplo.js
@@ -2,5 +2,12 @@ function holaMundo() {
   return "Hola Mundo";
 }
 
- const mensaje = holaMundo();
- console.log(mensaje);
+const mensaje = holaMundo();
+console.log(mensaje);
+
+function saludar(nombre) {
+  return `Hola ${nombre}`;
+}
+
+const saludo = saludar("Roberto");
+console.log(saludo);
